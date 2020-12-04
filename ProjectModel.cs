750e1789a8159cfe776a694379b28fd65bc89238
OyaEr <56042138+OyaEr@users.noreply.github.com>
2020-12-05 01:58:03 +0300

    Project tablosu oluşturuldu.

---
+++ ProjectModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bil372_Project.Models
+{
+    public class ProjectModel
+    {
+        [Key]
+        public int pid { get; set; }
+        public string p_adı { get; set; }
+        public int dno { get; set; }
+
+
+        [ForeignKey("dno")]
+
+        public Department Deparment { get; set; }
+
+
+    }
+}
