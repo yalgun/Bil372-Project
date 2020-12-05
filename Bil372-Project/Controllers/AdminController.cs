f916dd87e03bccb3f1054649a73fd83a1e52ca4c
SukranSaygili <gsukransaygili@gmail.com>
2020-12-05 23:23:07 +0300

    Register page front-end added

--- Bil372-Project/Controllers/AdminController.cs
+++ Bil372-Project/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bil372_Project.Data;
+using Bil372_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,35 +14,34 @@ namespace Bil372_Project.Controllers
         public ActionResult Index()
         {
             return View();
-=======
-using Bil372_Project.Data;
-using Bil372_Project.Models;
 
-namespace Bil372_Project.Controllers
-{
-    public class RegisterController : Controller
-    {
-
-        private DatabaseContext DbContext = new DatabaseContext();
-
-        public ActionResult RegisterPage()
-        {
-            return View ();
         }
 
-        public ActionResult Register(PersonelModel personelModel)
+
+        public class RegisterController : Controller
         {
 
-            if (!ModelState.IsValid)
+            private DatabaseContext DbContext = new DatabaseContext();
+
+            public ActionResult RegisterPage()
             {
-                return RegisterPage();
+                return View();
             }
 
-            DbContext.Personel.Add(personelModel);
-            DbContext.SaveChanges();
+            public ActionResult Register(PersonelModel personelModel)
+            {
 
-            return RedirectToAction("LoginPage", "Login");
->>>>>>> ea655b2... Register page back end added:Bil372-Project/Controllers/RegisterController.cs
+                if (!ModelState.IsValid)
+                {
+                    return RegisterPage();
+                }
+
+                DbContext.Personel.Add(personelModel);
+                DbContext.SaveChanges();
+
+                return RedirectToAction("LoginPage", "Login");
+
+            }
         }
     }
 }
