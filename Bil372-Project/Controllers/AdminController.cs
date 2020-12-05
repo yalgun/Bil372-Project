43c15bf2175caf33b8a6b7d0506e49c67646a73a
yalgun <43279137+yalgun@users.noreply.github.com>
2020-12-05 23:00:25 +0300

    bug fixed.

--- Bil372-Project/Controllers/AdminController.cs
+++ Bil372-Project/Controllers/AdminController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-<<<<<<< HEAD:Bil372-Project/Controllers/AdminController.cs
 
 namespace Bil372_Project.Controllers
 {
@@ -13,35 +12,7 @@ namespace Bil372_Project.Controllers
         public ActionResult Index()
         {
             return View();
-=======
-using Bil372_Project.Data;
-using Bil372_Project.Models;
-
-namespace Bil372_Project.Controllers
-{
-    public class RegisterController : Controller
-    {
-
-        private DatabaseContext DbContext = new DatabaseContext();
-
-        public ActionResult RegisterPage()
-        {
-            return View ();
-        }
-
-        public ActionResult Register(PersonelModel personelModel)
-        {
-
-            if (!ModelState.IsValid)
-            {
-                return RegisterPage();
-            }
-
-            DbContext.Personel.Add(personelModel);
-            DbContext.SaveChanges();
-
-            return RedirectToAction("LoginPage", "Login");
->>>>>>> ea655b2... Register page back end added:Bil372-Project/Controllers/RegisterController.cs
         }
     }
+
 }
