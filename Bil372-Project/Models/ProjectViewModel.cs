5ea5659a7a7e9bd50b14e0f4b5de0cdce5240541
OyaEr <56042138+OyaEr@users.noreply.github.com>
2020-12-05 20:37:57 +0300

    Bugfix

---
+++ Bil372-Project/Models/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bil372_Project.Models
+{
+    public class ProjectViewModel
+    {
+        public List<ProjectModel> Project { get; set; }
+        public ProjectViewModel()
+        {
+            Project = new List<ProjectModel>();
+        }
+    }
