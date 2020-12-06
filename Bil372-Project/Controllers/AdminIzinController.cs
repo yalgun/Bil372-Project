a6987a47135fbf250016ad47ed56f5e4536ed245
yalgun <43279137+yalgun@users.noreply.github.com>
2020-12-06 18:52:07 +0300

    Admin için AdminIzinController eklendi.

---
+++ Bil372-Project/Controllers/AdminIzinController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil372_Project.Controllers
+{
+    public class AdminIzinController : Controller
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+        // GET: AdminIzin
+        public ActionResult Index()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM [dbo].[IzinModel]", sqlCon);
+                sqlDA.Fill(dataTable);
+            }
+
+            return View(dataTable);
+        }
+    }
+}
