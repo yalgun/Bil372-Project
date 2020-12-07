5cadb435454d60a1384c972ebf07db711d15d833
OyaEr <56042138+OyaEr@users.noreply.github.com>
2020-12-07 05:43:37 +0300

    İş ilanı için controller,model,view.

---
+++ Bil372-Project/Controllers/Acik_PozisyonController.cs
@@ -0,0 +1,36 @@
+﻿using Bil372_Project.Data;
+using Bil372_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil372_Project.Controllers
+
+{
+    public class Acik_PozisyonController : Controller
+     {
+    private DatabaseContext DbContext = new DatabaseContext();
+    // GET: Acik_Pozisyon
+    public ActionResult Acik_Pozisyon()
+    {
+          
+        var viewModel = new Acik_PozisyonViewModel();
+        viewModel.Acik_PozisyonModels = DbContext.Acik_PozisyonModels.ToList();
+
+        ViewData["Message"] = viewModel;
+
+        return View(viewModel);
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
--- Bil372-Project/Models/Acik_PozisyonModel.cs
+++ Bil372-Project/Models/Acik_PozisyonModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
+
 namespace Bil372_Project.Models
 {
     public class Acik_PozisyonModel
