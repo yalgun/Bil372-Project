b67e3355ad8ac8c85ed6ac6ac8f011ae7084003a
yalgun <43279137+yalgun@users.noreply.github.com>
2020-12-08 01:27:23 +0300

    Department CRUD işlemleri ve viewleri tamamlandı.

--- Bil372-Project/Controllers/DepartmentController.cs
+++ Bil372-Project/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bil372_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -35,6 +36,60 @@ namespace Bil372_Project.Controllers
                 SqlCommand sqlCommand = new SqlCommand(query, sqlCon);
                 sqlCommand.ExecuteNonQuery();
 
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Department());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "INSERT INTO [dbo].[Department] VALUES('" + department.DepartmentName + "', '" + department.DepartmentAdress + "')";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlCon);
+                sqlCommand.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int DepartmentID)
+        {
+            Department department= new Department();
+            DataTable dtable = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "SELECT * FROM [dbo].[DepartmenT] WHERE DepartmentID ='" + DepartmentID + "'";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.Fill(dtable);
+                if (dtable.Rows.Count == 1)
+                {
+                    department.DepartmentID = (int)dtable.Rows[0][0];
+                    department.DepartmentName = dtable.Rows[0][1].ToString();
+                    department.DepartmentAdress = dtable.Rows[0][2].ToString();
+                    
+                    return View(department);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Department department)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "UPDATE [dbo].[Department] SET DepartmentName = '" + department.DepartmentName + "', DepartmentAdress = '" + department.DepartmentAdress+ "' WHERE DepartmentID = " + department.DepartmentID;
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.ExecuteNonQuery();
             }
             return RedirectToAction("Index");
         }
