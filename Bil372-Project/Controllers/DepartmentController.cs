9b1c2e9d6642596766e137a69f05aab271cc7c9e
yalgun <43279137+yalgun@users.noreply.github.com>
2020-12-08 00:07:19 +0300

    Admin Department listeleme ekranı ve departmanı silme işlemi eklendi.

---
+++ Bil372-Project/Controllers/DepartmentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil372_Project.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+        // GET: Department
+        public ActionResult Index()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM [dbo].[Department] ", sqlCon);
+                sqlDA.Fill(dataTable);
+            }
+
+            return View(dataTable);
+        }
+
+        public ActionResult Delete(int DepartmentID)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "DELETE FROM [dbo].[Department] WHERE DepartmentID = '" + DepartmentID + "'";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlCon);
+                sqlCommand.ExecuteNonQuery();
+
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
