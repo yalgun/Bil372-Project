96b47a35ffa071f30d92a6e80f85551d60b5dbca
SukranSaygili <gsukransaygili@gmail.com>
2020-12-07 23:18:07 +0300

    Maaş ekleme ekranı güncellendi

--- Bil372-Project/Controllers/AdminMaasController.cs
+++ Bil372-Project/Controllers/AdminMaasController.cs
@@ -2,9 +2,7 @@
 using Bil372_Project.Models;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +12,7 @@ namespace Bil372_Project.Controllers
     public class AdminMaasController : Controller
     {
         private DatabaseContext DbContext = new DatabaseContext();
-        public static string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
 
         public ActionResult AdminMaasPage()
         {
@@ -23,18 +21,57 @@ namespace Bil372_Project.Controllers
         }
 
         [HttpPost]
-        public ActionResult AdminMaasPage(SalaryModel salary)
+        public ActionResult getSelectedValue(SalaryModel salaryModel)
         {
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+
+            var person = Request.Form["Persons"];
+
+            int pId = Int16.Parse(person);
+            // DateTime giris = Request.Form["giris_saati"];
+            DbContext.SalaryModels.Add(new SalaryModel
             {
-                sqlCon.Open();
-                string query = "INSERT INTO [dbo].[SalaryModel] VALUES(" + salary.pid + ", '" + salary.miktar +
-                    "', '" + salary.tarih + "')";
-                SqlCommand sqlCommand = new SqlCommand(query, sqlCon);
-                sqlCommand.ExecuteNonQuery();
-            }
-            return RedirectToAction("AdminMaasPage");
+                pid = pId,
+                miktar = salaryModel.miktar,
+                tarih = salaryModel.tarih
+            });
+            DbContext.SaveChanges();
+
+            Learn_DevelopmentModel ld = new Learn_DevelopmentModel();
+
+            return RedirectToAction("ListAllSalary", "AdminMaasPage");
         }
+
+
+        public ActionResult ListAllSalary()
+        {
+            var viewModel = new SalaryViewModel();
+
+            viewModel.SalaryModels = DbContext.SalaryModels.ToList();
+
+            ViewData["Message"] = DbContext.SalaryModels.ToList();
+
+
+            var LD = DbContext.SalaryModels.Include(c => c.PersonModel).ToList();
+
+
+            return View(LD);
+
+        }
+
+        public ActionResult DeleteAttandance(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            DbContext.SalaryModels.Remove(DbContext.SalaryModels.Find(id));
+            DbContext.SaveChanges();
+
+            return RedirectToAction("ListAllTimeAttandance", "AdminMaasPage");
+
+        }
+
+
 
 
     }
